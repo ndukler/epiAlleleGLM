28039970429b64ee0fdc7a76d8d906d50daadc98
Noah <ned36@cornell.edu>
2018-08-21 19:00:56 -0400

    Began reworking rateModel class to encapsulate the data instead of the parameters for better behavior with "=" operator

--- R/rateModelValidityCheck.R
+++ R/rateModelValidityCheck.R
@@ -1,6 +1,11 @@
 #' @description  Validity checker for alleleData-class
 rateModelValidityCheck <- function(object){
   errors=c()
+  if(!setequal(names(object@alleleData),c("alleleData"))){
+    errors=c(errors,"object@alleleData must have one binding, \'alleleData\'") 
+  } else if(class(object@alleleData$alleleData)!="alleleData"){
+    errors=c(errors,"object@alleleData$alleleData is not of class allele Data")
+  }
   if(!setequal(colnames(object@edgeGroups),c("parent","child","edgeID","edgeGroup"))){
     errors=c(errors,"Colnames of edgeGroups must be \'parent\', \'child\', \'edgeID\',\'edgeGroup\'")
   }
@@ -10,34 +15,26 @@ rateModelValidityCheck <- function(object){
   if(!setequal(colnames(object@siteLabels),"siteLabel")){
     errors=c(errors,"object@siteLabels must have only one column, \'siteLabel\'")  
   }
-  if(!setequal(names(object@paramEnviron),c("params","paramIndex","fixed","logLikelihood"))){
-    errors=c(errors,"object@paramEnviron must have three slots, \'params\', \'paramIndex\', \'fixed\',\'logLikelihood\'") 
-  } else {
-    ## Checks for paramIndex in environment
-    if(!data.table::is.data.table(object@paramEnviron$paramIndex)){
-      errors=c(errors,"object@paramEnviron$paramIndex must be a data.table") 
-    } else if(!setequal(colnames(object@paramEnviron$paramIndex),c("edgeGroup","siteLabel","rateIndex","piIndex"))){
-      errors=c(errors,"object@paramEnviron$paramIndex must have columns \'edgeGroup\', \'siteLabel\', \'rateIndex\', \'piIndex\'")
-    } else if(!setequal(data.table::key(object@paramEnviron$paramIndex),c("edgeGroup","siteLabel"))){
-      errors=c(errors,"object@paramEnviron$paramIndex must have keys \'edgeGroup\', \'siteLabel\'")
-    }
-    ## Check for the params
-    expLen= max(object@paramEnviron$paramIndex$piIndex)+object@alleleData@nAlleles-1 ## expected length of parameter vec
-    if(!is.numeric(object@paramEnviron$params) || length(object@paramEnviron$params) != expLen){
-      errors=c(errors,paste("object@paramEnviron$params must be a numeric vector of length",expLen)) 
-    }
-    ## Checks for the fixed vector
-    if(!is.logical(object@paramEnviron$fixed) || length(object@paramEnviron$fixed) != expLen){
-      errors=c(errors,paste("object@paramEnviron$fixed must be a logical vector of length",expLen)) 
-    }
-    ## Checks for the logLikelihood vector
-    if(!is.numeric(object@paramEnviron$logLikelihood) || length(object@paramEnviron$logLikelihood) != 1){
-      errors=c(errors,"object@paramEnviron$logLikelihood must be a numeric vector of length one") 
-    }
+
+  ## Checks for paramIndex 
+  if(!setequal(colnames(object@paramIndex),c("edgeGroup","siteLabel","rateIndex","piIndex"))){
+    errors=c(errors,"object@paramIndex must have columns \'edgeGroup\', \'siteLabel\', \'rateIndex\', \'piIndex\'")
+  } else if(!setequal(data.table::key(object@paramIndex),c("edgeGroup","siteLabel"))){
+    errors=c(errors,"object@paramIndex must have keys \'edgeGroup\', \'siteLabel\'")
   }
+  ## Check for the params
+  expLen= max(object@paramIndex$piIndex)+object@alleleData$alleleData@nAlleles-1 ## expected length of parameter vec
+  if(!is.numeric(object@params) || length(object@params) != expLen){
+    errors=c(errors,paste("object@params must be a numeric vector of length",expLen)) 
+  }
+  ## Checks for the fixed vector
+  if(!is.logical(object@fixed) || length(object@fixed) != expLen){
+    errors=c(errors,paste("object@fixed must be a logical vector of length",expLen)) 
+  }
+
   ## Lock the environment and all the bindings in it if all tests passed
   if (length(errors) == 0){
-    lockEnvironment(env = object@paramEnviron,bindings = TRUE)
+    lockEnvironment(env = object@alleleData,bindings = TRUE)
   }
   ## Return errors if there are any
   if (length(errors) == 0) TRUE else errors
