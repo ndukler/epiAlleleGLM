4ae82c1f0371ba6ac92cba6746b13a541286f562
Noah <ned36@cornell.edu>
2018-08-21 23:15:43 -0400

    Updated setParam method

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -15,9 +15,22 @@ BEGIN_RCPP
     return rcpp_result_gen;
 END_RCPP
 }
+// setValues
+void setValues(NumericVector& x, NumericVector& ind, NumericVector& val);
+RcppExport SEXP _epiAllele_setValues(SEXP xSEXP, SEXP indSEXP, SEXP valSEXP) {
+BEGIN_RCPP
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< NumericVector& >::type x(xSEXP);
+    Rcpp::traits::input_parameter< NumericVector& >::type ind(indSEXP);
+    Rcpp::traits::input_parameter< NumericVector& >::type val(valSEXP);
+    setValues(x, ind, val);
+    return R_NilValue;
+END_RCPP
+}
 
 static const R_CallMethodDef CallEntries[] = {
     {"_epiAllele_rcpp_hello_world", (DL_FUNC) &_epiAllele_rcpp_hello_world, 0},
+    {"_epiAllele_setValues", (DL_FUNC) &_epiAllele_setValues, 3},
     {NULL, NULL, 0}
 };
 
---
+++ src/setValues.cpp
@@ -0,0 +1,9 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+// [[Rcpp::export]]
+void setValues(NumericVector& x, NumericVector& ind, NumericVector& val) {
+  for(int i=0; i<ind.size();i++){
+    x(ind(i))=val(i);
+  }
+}
