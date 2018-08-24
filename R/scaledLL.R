4f0d97911a193f66a66cadac3c894027c0313153
Noah <ned36@cornell.edu>
2018-08-24 13:28:27 -0400

    Add function that returns scaled log-likelihood

---
+++ R/scaledLL.R
@@ -0,0 +1,27 @@
+#' Compute scaled negative log-likelihood
+#'
+#' Compute scaled negative log-likelihood for given rate model object
+#' @param x a set of parameters to compute the log-likelihood for
+#' @param obj rateModel object
+#' @param stickParams
+#' @return A rateModel object with updated parameter values
+#' @name scaledLL
+#' @include rateModel-class.R
+#' @rdname scaledLL
+methods::setGeneric("scaledLL", function(x,obj,scale,...) {
+  standardGeneric("scaledLL")
+})
+
+#' @name scaledLL
+#' @rdname scaledLL
+methods::setMethod("scaledLL", signature(x="missing",obj = "rateModel"), function(x,obj,scale) {
+  ll=logLikelihood(x = x,obj = obj)
+  return(ll*scale)
+})
+
+#' @name scaledLL
+#' @rdname scaledLL
+methods::setMethod("scaledLL", signature(x="numeric",obj = "rateModel"), function(x,obj,scale,stickParams=TRUE) {
+  ll=logLikelihood(x = x,obj = obj,stickParams = stickParams)
+  return(ll*scale)
+})
