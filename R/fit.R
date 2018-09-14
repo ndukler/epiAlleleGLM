6f69969c96a0f00b6345f7b9c33de10c03a8fd87
Noah <ned36@cornell.edu>
2018-09-14 15:08:43 -0400

    Added access to control options of optimizers

--- R/fit.R
+++ R/fit.R
@@ -1,4 +1,4 @@
-methods::setGeneric("fit", function(obj,scale=NULL,method=c("l-bfgs-b","mlsl","stogo")) {
+methods::setGeneric("fit", function(obj,scale=NULL,method=c("l-bfgs-b","mlsl","stogo"),control=list()) {
   standardGeneric("fit")
 })
 
@@ -8,13 +8,14 @@ methods::setGeneric("fit", function(obj,scale=NULL,method=c("l-bfgs-b","mlsl","s
 #' @param obj rateModel
 #' @param scale a scale factor to apply to log-likelihood, defaults to -1/nsites
 #' @param method Optimization method to use ("l-bfgs-b","mlsl","stogo")
+#' @param control See control from \link[stats]{optim} if using l-bfgs-b, otherwise look at \link[nloptr]{nl.opts}
 #' @name fit
 #' @return a list incuding a rateModel object and information about the optimization
 #' @include rateModel-class.R
 #' @examples
 #' 
 #' @export
-methods::setMethod("fit", signature(obj = "rateModel"), function(obj,scale=NULL,method=c("l-bfgs-b","mlsl","stogo")) {
+methods::setMethod("fit", signature(obj = "rateModel"), function(obj,scale=NULL,method=c("l-bfgs-b","mlsl","stogo"),control=list()) {
   ## scale defaults to -1/nsites
   if(is.null(scale)){
     sca=-1/nrow(getAlleleData(obj)@data)
@@ -39,15 +40,29 @@ methods::setMethod("fit", signature(obj = "rateModel"), function(obj,scale=NULL,
   ## Where the parameter values are fixed, set lb=ub=value
   ## ub[obj@fixed]=x[obj@fixed]
   ## lb[obj@fixed]=x[obj@fixed]
+  
+  ## Set default control options
+  if(method %in% c("l-bfgs-b")){
+    cont = list(ndeps=rep(10^-6,length(y)))
+  } else {
+    cont = nloptr::nl.opts()
+  }
+  ## Overwrite defaults when user has supplied values
+  for(n in names(control)){
+    cont[[n]]=control[[n]]
+  } 
+  
   obj2=clone(obj,deep=FALSE)
   if(method=="l-bfgs-b"){
     optMod=optim(y,fn = epiAllele:::scaledLL,obj=obj2,scale=sca,stickParams=TRUE,lower = lb,upper = ub,method="L-BFGS-B",
-               control = list(ndeps=rep(10^-6,length(y))))
+               control = cont)
   } else if(method == "mlsl"){
-    optMod=nloptr::mlsl(x0=y,fn = epiAllele:::scaledLL,lower = lb,upper = ub,obj=obj2,scale=sca,stickParams=TRUE)
+    optMod=nloptr::mlsl(x0=y,fn = epiAllele:::scaledLL,lower = lb,upper = ub,obj=obj2,scale=sca,stickParams=TRUE,
+                        control = cont)
     counts=optMod$iter
   } else if(method == "stogo"){
-    optMod=nloptr::stogo(x0=y,fn = epiAllele:::scaledLL,lower = lb,upper = ub,obj=obj2,scale=sca,stickParams=TRUE)
+    optMod=nloptr::stogo(x0=y,fn = epiAllele:::scaledLL,lower = lb,upper = ub,obj=obj2,scale=sca,stickParams=TRUE,
+                         control = cont)
     counts=optMod$iter
   } else {
     stop("Invalid optimization method specified")
