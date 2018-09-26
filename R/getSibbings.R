b0844b500af616d20c4f279d6061aad192f3f5e9
Noah <ned36@cornell.edu>
2018-09-26 16:55:30 -0400

    Function to get all sibblings of a node

---
+++ R/getSibbings.R
@@ -0,0 +1,19 @@
+#' Compute transitions per branch
+#'
+#' Compute the number of expected transitions per branch between states based on the marginal joint probabilities
+#' @param tr phylo object
+#' @param node
+#' @return The node numbers of sibbling nodes
+#' @name getSibblings
+#' @include rateModel-class.R
+#' @rdname getSibblings
+#' @examples
+#' 
+#' @export
+getSibblings = function(tr,node){
+  if(!inherits(tr,"phylo")) {
+    stop("tree should be an object of class \"phylo\".")
+  }
+  parent=tr$edge[tr$edge[,2]==node,1]
+  return(setdiff(tr$edge[tr$edge[,1]==parent,2],node))
+}
