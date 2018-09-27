486a1457cbf4234efcf67dc1ea13fd0472f4e0eb
Noah <ned36@cornell.edu>
2018-09-26 22:17:00 -0400

    Structuring function to call traversals per site

--- src/marginalTransitionsCpp.cpp
+++ src/marginalTransitionsCpp.cpp
@@ -2,6 +2,7 @@
 // [[Rcpp::depends(RcppArmadillo)]]
 #include "logSumExp.h"
 #include "epiAllele_types.h"
+#include "msgPassing.h"
 
 // Add a flag to enable OpenMP at compile time
 // [[Rcpp::plugins(openmp)]]
@@ -17,7 +18,14 @@ arma::cube marginalTransitionsCpp(const NumericMatrix& data, const NumMatList& t
                                   const NumericVector& logPi,const NumVecList& siblings, int ncores = 1) {
   unsigned int nAlleles=logPi.size();
   // Create cube to hold the expected number of transitions over all sites
-  arma::cube expectedTransitions(traversal.nrow(),nAlleles,nAlleles,arma::fill::zeros);  
-  
-  
+  arma::cube expectedTransitions(traversal.nrow(),nAlleles,nAlleles,arma::fill::zeros);
+  unsigned int nNode=Rcpp::max(traversal(_,0))+1; // The total number of nodes on the tree
+  unsigned int root=traversal(traversal.nrow()-1,0); // Get the index of the root node
+  // Iterate over all sites
+  for(int i=0;i<data.nrow();i++){
+    // First do the forward message passing up to the root
+    arma::mat alpha=postorderMessagePassing((Rcpp::NumericVector) data(i,_),tMat,traversal,nTips,logPi,nNode);
+    arma::mat beta=preorderMessagePassing((Rcpp::NumericVector) data(i,_),tMat,traversal,nTips,logPi,siblings,nNode,root);
+  }
+  return(expectedTransitions);
 }
