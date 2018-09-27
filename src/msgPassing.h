15c055d81cde37f336c19af62c0aa04e39530929
Noah <ned36@cornell.edu>
2018-09-27 10:51:46 -0400

    Fixed signature type

--- src/msgPassing.h
+++ src/msgPassing.h
@@ -9,6 +9,6 @@ using namespace Rcpp;
 arma::mat postorderMessagePassing(const NumericVector& data, const NumMatList& tMat, const NumericMatrix& traversal, const double nTips, 
                                   const NumericVector& logPi, unsigned int nNode,int ncores = 1);
 arma::mat preorderMessagePassing(const NumericVector& data, const NumMatList& tMat, const NumericMatrix& traversal, const double nTips, 
-                                 const NumericVector& logPi,const NumericMatrix& alpha, const NumVecList& siblings, int nNode, int root, 
+                                 const NumericVector& logPi,const arma::mat& alpha, const NumVecList& siblings, int nNode, int root, 
                                  int ncores = 1);
 #endif
