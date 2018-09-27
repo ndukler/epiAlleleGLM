5603cc0538caceda3ef2cf8c560074c7f626817e
Noah <ned36@cornell.edu>
2018-09-26 22:15:59 -0400

    Msg passing algorithms now only work on a single site

--- src/postorderMessagePassing.cpp
+++ src/postorderMessagePassing.cpp
@@ -3,40 +3,26 @@
 #include "logSumExp.h"
 #include "epiAllele_types.h"
 
-// Add a flag to enable OpenMP at compile time
-// [[Rcpp::plugins(openmp)]]
-
-// Protect against compilers without OpenMP
-#ifdef _OPENMP
-#include <omp.h>
-#endif
-
-// Parallelized C++ equivalent
+// Forward message passing for a single site
 // [[Rcpp::export]]
-arma::cube postorderMessagePassing(const NumericMatrix& data, const NumMatList& tMat, const NumericMatrix& traversal, const double nTips, 
-                     const NumericVector& logPi,int ncores = 1) {
-  unsigned int sites=data.nrow();
+arma::mat postorderMessagePassing(const NumericVector& data, const NumMatList& tMat, const NumericMatrix& traversal, const double nTips, 
+                                  const NumericVector& logPi,unsigned int nNode,int ncores = 1) {
   unsigned int nAlleles=logPi.size();
-  unsigned int nNode=Rcpp::max(traversal(_,0))+1; // The total number of nodes on the tree
-  arma::cube poTab(sites,nNode,nAlleles,arma::fill::zeros);  
-    
-  // Added an omp pragma directive to parallelize the loop with ncores
-  #pragma omp parallel for num_threads(ncores)
-  for(unsigned int i=0;i<sites;i++){
-    // Initialize values for the tips of the tree
-    for(unsigned int n=0;n<nTips;n++){
-      for(unsigned int a=0;a<nAlleles;a++){
-        poTab(i,n,a)=data(i,(n*nAlleles)+a);
-      }
+  arma::mat poTab(nNode,nAlleles,arma::fill::zeros);  
+  
+  // Initialize values for the tips of the tree
+  for(unsigned int n=0;n<nTips;n++){
+    for(unsigned int a=0;a<nAlleles;a++){
+      poTab(n,a)=data((n*nAlleles)+a);
     }
-    // Now compute the probability for the interior nodes
-    for(unsigned int n=0;n<traversal.nrow();n++){
-      unsigned int parentInd=traversal(n,0);
-      unsigned int childInd=traversal(n,1);
-      arma::mat tMatA = as<arma::mat>(tMat[n]);
-      for(unsigned int a=0;a<nAlleles;a++){ // iterate over all parental alleles
-        poTab(i,parentInd,a) = poTab(i,parentInd,a) + logSumExpArma((arma::vec) poTab(arma::span(i), arma::span(childInd), arma::span::all)+ tMatA.row(a).t());
-      }
+  }
+  // Now compute the probability for the interior nodes
+  for(unsigned int n=0;n<traversal.nrow();n++){
+    unsigned int parentInd=traversal(n,0);
+    unsigned int childInd=traversal(n,1);
+    arma::mat tMatA = as<arma::mat>(tMat[n]);
+    for(unsigned int a=0;a<nAlleles;a++){ // iterate over all parental alleles
+      poTab(parentInd,a) = poTab(parentInd,a) + logSumExpArma((arma::vec) poTab(arma::span(childInd), arma::span::all)+ tMatA.row(a).t());
     }
   }
   return(poTab);
--- src/preorderMessagePassing.cpp
+++ src/preorderMessagePassing.cpp
@@ -3,39 +3,23 @@
 #include "logSumExp.h"
 #include "epiAllele_types.h"
 
-// Add a flag to enable OpenMP at compile time
-// [[Rcpp::plugins(openmp)]]
-
-// Protect against compilers without OpenMP
-#ifdef _OPENMP
-#include <omp.h>
-#endif
-
-// Parallelized C++ equivalent
 // [[Rcpp::export]]
-arma::cube preorderMessagePassing(const NumericMatrix& data, const NumMatList& tMat, const NumericMatrix& traversal, const double nTips, 
-                                   const NumericVector& logPi,const NumVecList& siblings, int ncores = 1) {
-  unsigned int sites=data.nrow();
+arma::mat preorderMessagePassing(const NumericVector& data, const NumMatList& tMat, const NumericMatrix& traversal, const double nTips, 
+                                   const NumericVector& logPi,const NumVecList& siblings, int nNode, int root, int ncores = 1) {
   unsigned int nAlleles=logPi.size();
-  unsigned int nNode=Rcpp::max(traversal(_,0))+1; // The total number of nodes on the tree
-  arma::cube poTab(sites,nNode,nAlleles,arma::fill::zeros);  
+  arma::mat poTab(nNode,nAlleles,arma::fill::zeros);  
   
-  unsigned int root=traversal(traversal.nrow()-1,0); // Get the index of the root node
-  // Added an omp pragma directive to parallelize the loop with ncores
-  #pragma omp parallel for num_threads(ncores)
-  for(unsigned int i=0;i<sites;i++){
-    // Initialize the root
-    for(unsigned int a=0;a<nAlleles;a++){
-      poTab(i,root,a)=logPi(a);
-    }
-    // Now compute the probability for the interior nodes
-    for(int n=traversal.nrow()-1;n>=0;n--){
-      unsigned int parentInd=traversal(n,0);
-      unsigned int childInd=traversal(n,1);
-      arma::mat tMatA = as<arma::mat>(tMat[n]); 
-      for(unsigned int a=0;a<nAlleles;a++){ // iterate over all parental alleles
-        poTab(i,childInd,a) = logSumExpArma((arma::vec) poTab(arma::span(i), arma::span(parentInd), arma::span::all) + tMatA.col(a));
-      }
+  // Initialize the root
+  for(unsigned int a=0;a<nAlleles;a++){
+      poTab(root,a)=logPi(a);
+  }
+  // Now compute the probability for the interior nodes
+  for(int n=traversal.nrow()-1;n>=0;n--){
+    unsigned int parentInd=traversal(n,0);
+    unsigned int childInd=traversal(n,1);
+    arma::mat tMatA = as<arma::mat>(tMat[n]); 
+    for(unsigned int a=0;a<nAlleles;a++){ // iterate over all parental alleles
+      poTab(childInd,a) = logSumExpArma((arma::vec) poTab(arma::span(parentInd), arma::span::all) + tMatA.col(a));
     }
   }
   return(poTab);
