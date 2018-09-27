381c9afe9985481a22d5eb5b885a3ae97a4be32e
Noah <ned36@cornell.edu>
2018-09-26 22:19:37 -0400

    Parallelize over sites

--- src/marginalTransitionsCpp.cpp
+++ src/marginalTransitionsCpp.cpp
@@ -22,6 +22,7 @@ arma::cube marginalTransitionsCpp(const NumericMatrix& data, const NumMatList& t
   unsigned int nNode=Rcpp::max(traversal(_,0))+1; // The total number of nodes on the tree
   unsigned int root=traversal(traversal.nrow()-1,0); // Get the index of the root node
   // Iterate over all sites
+  #pragma omp parallel for num_threads(ncores)
   for(int i=0;i<data.nrow();i++){
     // First do the forward message passing up to the root
     arma::mat alpha=postorderMessagePassing((Rcpp::NumericVector) data(i,_),tMat,traversal,nTips,logPi,nNode);
